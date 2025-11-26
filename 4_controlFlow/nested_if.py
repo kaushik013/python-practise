19e8e3223d1b865e9b6fa87dbdc3a785e53714e6
kaushik013 <chariyakaushik1435@gmail.com>
2025-11-26 23:42:08 +0530

    new file

---
+++ 4_controlFlow/nested_if.py
@@ -0,0 +1,28 @@
+
+
+# ⁡⁢⁣⁣1. Check if a number is positive, then check if it's even or odd. 
+# 2. Check if a number is negative, then check if it's a multiple of 5. 
+# 3. Check if a person is eligible to vote, then check if they are a senior citizen. 
+# 4. Check if a number is greater than 50, then check if it's divisible by 10. 
+# 5. Check if a given character is an alphabet, then check if it's uppercase or 
+# lowercase. 
+# 6. Check if a student has passed, then check if they scored distinction (>=75). 
+# 7. Check if a year is a leap year, then check if it is a century year. 
+# 8. Check if a person is eligible for a discount, then check if the purchase is above 
+# 10,000.  
+# 10. Check if a person’s age is above 18, then check if they can drive a car. 
+# 11. Check if an entered username is correct, then check if the password is correct. 
+# 12. Check if a triangle is valid, then check if it's equilateral, isosceles, or scalene. 
+# 13. Check if a student passed, then check if they got a scholarship (>90 marks). 
+# 14. Check if a person is tall, then check if they are also underweight. 
+# 15. Check if a number is within a range, then check if it is odd or even. 
+# 16. Check if a product is in stock, then check if it is on sale. 
+# 17. Check if a bank account balance is sufficient, then check if withdrawal is possible. 
+# 18. Check if a given input is a number, then check if it's positive or negative.⁡
+
+
+
+# ⁡⁢⁣⁣1. Check if a number is positive, then check if it's even or odd. 
+
+a = int(input('enter the number : '))
+
