b69789359502b929d83efb98200267042d7c9966
kaushik013 <chariyakaushik1435@gmail.com>
2026-01-23 18:13:17 +0530

    file handling

---
+++ 103_file_handling/1_file.py
@@ -0,0 +1,31 @@
+
+# with open('file1','w') as writ:
+#     writ.writelines(['python datatype...\n','1. int\n','2 float'])
+
+#! readline()
+# with open('file1','r') as readi:
+#     result = readi.readline()
+#     result = readi.readline()
+
+#     print(result)
+
+
+#! readlines()
+# with open('file1','r') as rid:
+#     result = rid.readlines()
+#     print(result)
+
+
+
+
+# writ.seek(0)
+# curser in firsat 
+
+# write+ (w+)
+# with open('biodata','w+') as writ:
+#     writ.writelines(['Bio-Data\n','i am kaushik\n','i am from Girsomnath\n','i am 22 years old\n','i am Data Scientist'])
+#     writ.seek(0)
+#     result = writ.read()
+#     print(result)
+
+
